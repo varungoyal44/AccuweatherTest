af452d755b6828d8f2bb8947c101841d4a08365e
Varun Goyal <varun_v_goyal@dell.com>
2015-03-13 23:56:57 -0400

    first commit

---
+++ AccuTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AccuTest
+//
+//  Created by Varun Goyal on 3/13/15.
+//  Copyright (c) 2015 Accuweather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
